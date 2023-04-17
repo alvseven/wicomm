76e463c315684ceff5605a838f1392ac74f1772c
alvseven <alv7contato@outlook.com>
2023-04-17 17:35:42 -0300

    feat: products with discount  component

---
+++ src/components/ProductsWithDiscountSection/index.jsx
@@ -0,0 +1,9 @@
+import { ProductsRelease, Section } from "../../components";
+
+export const ProductsWithDiscountSection = () => {
+  return (
+    <Section padding="125px 0 0 0">
+      <ProductsRelease label="PROMOÇÕES" />
+    </Section>
+  );
+};

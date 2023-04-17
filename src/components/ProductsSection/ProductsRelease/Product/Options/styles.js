d5e18264f616a29b31ebf4ee7b09d1eb752f9cc4
alvseven <alv7contato@outlook.com>
2023-04-17 20:03:42 -0300

    fix: removing options component

--- src/components/ProductsSection/ProductsRelease/Product/Options/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const BuyContainer = styled.div``;
-export const TastesContainer = styled.div``;
-export const ColorsContainer = styled.div``;
-export const OptionsContainer = styled.div``;
